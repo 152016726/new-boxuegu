581f95fc122597332302e9c9f1644d9c703f921a
roger <1520196726@qq.com>
2017-09-15 21:34:29 +0800

    富文本插件和三级联动插件安装但有提交BUG

--- src/js/user/profile.js
+++ src/js/user/profile.js
@@ -22,12 +22,13 @@ $('#teacher-profile-form').ajaxForm({
             }
         },
         error:function () {
-            alert('登录失败');
+            alert('保存失败');
         }
 });
 
 //所有的插件的初始化
 function initPlugin() {
+    //日期插件
     $('input[name=tc_birthday]').datepicker({
         language:'zh-CN',
         format:'yyyy-mm-dd',
@@ -37,5 +38,14 @@ function initPlugin() {
         language:'zh-CN',
         format:'yyyy-mm-dd',
         endDate:new Date('2017-9-16')
+    });
+    //三级联动
+    $('#region-container').region({
+        url: '/lib/jquery-region/region.json'
+    });
+    //富文本编辑器
+    CKEDITOR.replace("introduce", {
+        width:600,
+        skin:"kama"
     });
 }
